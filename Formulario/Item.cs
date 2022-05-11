f28e4702a858c47999709e6e70e76f357f266a42
David Serrano <franklinserrano23@gmail.com>
2022-05-11 00:32:51 -0500

    1.0 - Incompleto

---
+++ Formulario/Item.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace Formulario
+{
+    public class Item : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        string fullname = string.Empty;
+
+        public string Fullname 
+        {
+            get => fullname; 
+            set
+            {
+                if (fullname == value) return;
+
+                fullname = value;
+                onPropertyChanged(nameof(Fullname));
+                onPropertyChanged(nameof(showName));
+            }
+        }
+
+        public string showName => $"{Fullname}";
+
+        void onPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+
+    public class ListItem
+    {
+        // public List<ListItem> _items
+        // {
+        //     get;
+        //     set;
+        // }
+
+        // public ListItem()
+        // {
+        //     _items = new List<Item>();
+        //     loadItems();
+        // }
+
+        // public void loadItems()
+        // {
+        //     _items.Add(new Item
+        //     {
+        //         Name = ""
+        //     });
+        // }
+    }
+}
---
+++ Formulario/Login.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Formulario
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_login_Clicked(object sender, EventArgs e)
+        {
+            String Usuario = User.Text;
+            String Contraseña = Password.Text;
+
+            if (Usuario == "fserrano" && Contraseña == "61734")
+            {
+                Navigation.PushAsync(new Dashboard());
+            }
+            else
+            {
+                DisplayAlert("Error", "Wrong username and/or password", "Retry");
+            }
+        }
+    }
+}
